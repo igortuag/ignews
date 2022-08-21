3a5943adfe6f674c1956aae08c0584ee768e2052
Tuag <igortuag@gmail.com>
2022-08-21 10:04:10 -0300

    add cascadian style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
 import styles from "../../styles/home.module.scss";
 
 export default function Home() {
-  return <h1 className={styles.title}>Ig news</h1>;
+  return (
+    <h1 className={styles.title}>
+      Ig
+      <span> news</span>
+    </h1>
+  );
 }
