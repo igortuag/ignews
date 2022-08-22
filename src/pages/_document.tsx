9572236859d5fa79724cc089dc184287ba6c74b2
Tuag <igortuag@gmail.com>
2022-08-22 09:08:23 -0300

    add font roboto

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,400;0,700;0,900;1,700;1,900&family=Roboto:wght@400;500;700;900&family=Spectral:wght@700&display=swap"
+            rel="stylesheet"
+          />
           <title>ig.news</title>
         </Head>
         <body>
