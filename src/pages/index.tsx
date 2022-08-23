39b81b2caa6822e9d41f1b2d080213b3ecaeab83
Tuag <igortuag@gmail.com>
2022-08-23 08:38:23 -0300

    add title on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,17 @@
 import styles from "../../styles/home.module.scss";
+import { Head } from "next/document";
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Ig
-      <span> news</span>
-    </h1>
+    <div>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <h1 className={styles.title}>
+        Ig
+        <span> news</span>
+      </h1>
+    </div>
   );
 }
