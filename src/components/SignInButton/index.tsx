34ae8286eb1a32f354d54e7bd08795fbe3794c2d
Tuag <igortuag@gmail.com>
2022-08-28 07:11:57 -0300

    add logged button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,21 @@
 import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
 import styles from "./styles.module.scss";
 
 export default function SignInButton() {
+  const isUserLoggedIn = true;
+
+  if (isUserLoggedIn) {
+    return (
+      <button className={styles.signInButton}>
+        <FaGithub color="#04d361" />
+        Igor Tuag
+        <FiX color="#737380" className={styles.closeIcon} />
+      </button>
+    );
+  }
+
   return (
     <button className={styles.signInButton}>
       <FaGithub color="#eba417" />
