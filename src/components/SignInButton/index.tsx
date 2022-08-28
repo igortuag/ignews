bb743f3e288220fac66a77486be5f14ad1e070d8
Tuag <igortuag@gmail.com>
2022-08-28 07:07:47 -0300

    add styles to button

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,8 +1,9 @@
 import { FaGithub } from "react-icons/fa";
+import styles from "./styles.module.scss";
 
 export default function SignInButton() {
   return (
-    <button>
+    <button className={styles.signInButton}>
       <FaGithub />
       Sign in with Github
     </button>
