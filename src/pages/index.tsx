b8082cd7eca186446c75f9a62b67e838988d3031
Tuag <igortuag@gmail.com>
2022-09-04 09:05:09 -0300

    change page generation to static

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import SubscribeButton from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
@@ -30,7 +30,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1LdBoGDh99IwFTOfPZHwDbhd", {
     expand: ["product"],
   });
