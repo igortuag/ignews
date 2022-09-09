10679308ab4c810bd39c17dffcb247b69d96adb3
Tuag <igortuag@gmail.com>
2022-09-09 08:49:52 -0300

    add session to button login

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,17 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
 export default function SignInButton() {
-  const isUserLoggedIn = false;
+  const { data: session } = useSession();
 
-  if (isUserLoggedIn) {
+  if (session) {
     return (
       <button className={styles.signInButton}>
         <FaGithub color="#04d361" />
-        Igor Tuag
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
       </button>
     );
