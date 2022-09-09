0b3332a38a010ac686f6add114b6d0d4888b51b2
Tuag <igortuag@gmail.com>
2022-09-09 08:50:24 -0300

    add method to logout user

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 import styles from "./styles.module.scss";
 
@@ -9,7 +9,7 @@ export default function SignInButton() {
 
   if (session) {
     return (
-      <button className={styles.signInButton}>
+      <button className={styles.signInButton} onClick={() => signOut()}>
         <FaGithub color="#04d361" />
         {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
