98b9c9b4afa290e5c391b645272e58f6e75b8fe0
Tuag <igortuag@gmail.com>
2022-09-13 08:24:34 -0300

    add function handleSubscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -7,8 +7,14 @@ interface SubscribeButtonProps {
 }
 
 export default function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  function handleSubscribe() {}
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
