6dc73cbedb9c7cabbea188e31d9f2c85dcc06702
Tuag <igortuag@gmail.com>
2022-09-17 07:07:48 -0300

    get user from fauna

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,14 +1,36 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from "faunadb";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
 
 const Subscribe = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
-    const { user } = await getSession({ req });
+    const session = await getSession({ req });
+
+    const user = await fauna.query(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
 
     const stripeCustomer = await stripe.customers.create({
-      email: user.email,
+      email: session.user.email,
     });
+
+    await fauna.query(
+      q.Update(
+        q.Select(
+          "ref",
+          q.Get(
+            q.Match(q.Index("user_by_email"), q.Casefold(session.user.email))
+          )
+        ),
+        {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        }
+      )
+    );
 
     const { priceId } = req.body;
 
