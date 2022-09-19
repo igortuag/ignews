68d65f92ebc8c9a3199a8974e1f5ec5ae4dc62c6
Tuag <igortuag@gmail.com>
2022-09-19 08:52:20 -0300

    add buffer function

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,7 +1,21 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { Readable } from "stream";
+
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunk of readable) {
+    chunks.push(
+      typeof chunk === "string" ? Buffer.from(chunk) : chunk
+    );
+  }
+
+  return Buffer.concat(chunks);
+}
 
 const Webhooks = (req: NextApiRequest, res: NextApiResponse) => {
-  console.log(req.body);
+  
+  
   res.status(200).json({ message: "Hello" });
 };
 
