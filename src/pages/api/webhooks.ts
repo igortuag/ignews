12bb0ead1ac562cf5dbc83ed67bad67067a6deab
Tuag <igortuag@gmail.com>
2022-09-20 08:21:42 -0300

    add logic to construct event

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -1,25 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
+import { stripe } from "../../services/stripe";
 
 async function buffer(readable: Readable) {
   const chunks = [];
 
   for await (const chunk of readable) {
-    chunks.push(
-      typeof chunk === "string" ? Buffer.from(chunk) : chunk
-    );
+    chunks.push(typeof chunk === "string" ? Buffer.from(chunk) : chunk);
   }
 
   return Buffer.concat(chunks);
 }
 
-
 export const config = {
   api: {
     bodyParser: false,
-  }
-}
+  },
+};
 
 const Webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -27,7 +25,17 @@ const Webhooks = async (req: NextApiRequest, res: NextApiResponse) => {
     const secret = req.headers["stripe-signature"];
 
     let event: Stripe.Event;
-  
+
+    try {
+      event = stripe.webhooks.constructEvent(
+        buf,
+        secret,
+        process.env.STRIPE_WEBHOOK_SECRET
+      );
+    } catch (err) {
+      return res.status(400).send(`Webhook error: ${err.message}`);
+    }
+
     res.status(200).json({ message: "Hello" });
   } else {
     res.setHeader("Allow", "POST");
