4631612c65a76211b175206691006fd621ebce24
Tuag <igortuag@gmail.com>
2022-09-28 05:40:41 -0300

    add structure to page post

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,3 +1,16 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
 export default function Posts() {
-  return <div>Posts</div>;
+  return (
+    <>
+      <Head>
+        <title>Posts | ig.news</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div></div>
+      </main>
+    </>
+  );
 }
