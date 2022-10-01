656634dba811f5fb0b6263ca21b04ce3cc1f9c5a
Tuag <igortuag@gmail.com>
2022-10-01 06:11:07 -0300

    format posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Head from "next/head";
 import { getPrismicClient } from "../../services/prismic";
 import styles from "./styles.module.scss";
 import Prismic from "@prismicio/client";
+import { RichText } from "prismic-dom";
 
 export default function Posts() {
   return (
@@ -79,10 +80,27 @@ export const getStaticProps: GetStaticProps = async () => {
       pageSize: 100,
     }
   );
-  
+
+  const posts = response.results.map((post) => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find((content) => content.type === "paragraph")
+        .text,
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
+        "en-US",
+        {
+          day: "2-digit",
+          month: "long",
+          year: "numeric",
+        }
+      ),
+    };
+  });
+
   return {
     props: {
-      posts: response.results,
+      posts,
     },
   };
-}
+};
