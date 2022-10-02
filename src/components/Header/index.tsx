816fed623029e244c55afae5c68e35d2c4e20681
Tuag <igortuag@gmail.com>
2022-10-02 10:18:47 -0300

    add anchor to suport next link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,9 +9,12 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
           <Link href="/" className={styles.active}>
-            Home
+            <a>Home</a>
           </Link>
-          <Link href="/posts">Posts</Link>
+
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
 
         <SignInButton />
