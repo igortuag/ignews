5034d2584ba81a11acf3a8fd5ca459cbdd1a6dd9
Tuag <igortuag@gmail.com>
2022-10-03 08:39:56 -0300

    add structure to active link

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,7 +1,14 @@
-export function ActiveLink() {
+import Link, { LinkProps } from "next/link";
+
+interface ActiveLinkProps extends LinkProps {
+  children: React.ReactNode;
+  activeClassName: string;
+}
+
+export function ActiveLink({ children, href }: ActiveLinkProps) {
   return (
-    <div>
-      <h1>ActiveLink</h1>
-    </div>
+    <Link href={href}>
+      <a>{children}</a>
+    </Link>
   );
 }
