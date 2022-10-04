9f5c700547e3706f7b4e0180bcb506990076dbc2
Tuag <igortuag@gmail.com>
2022-10-04 08:40:13 -0300

    clone children to add class active

--- src/components/ActiveLink/index.tsx
+++ src/components/ActiveLink/index.tsx
@@ -1,8 +1,9 @@
 import Link, { LinkProps } from "next/link";
 import { useRouter } from "next/router";
+import { cloneElement, ReactElement } from "react";
 
 interface ActiveLinkProps extends LinkProps {
-  children: React.ReactNode;
+  children: ReactElement;
   activeClassName: string;
 }
 
@@ -12,12 +13,13 @@ export function ActiveLink({
   ...props
 }: ActiveLinkProps) {
   const { asPath } = useRouter();
+  const className = asPath === props.href ? activeClassName : "";
 
   return (
-    <Link {...props}
-      className={asPath === props.href ? activeClassName : ""}
-    >
-      {children}
+    <Link {...props}>
+      {cloneElement(children, {
+        className,
+      })}
     </Link>
   );
 }
