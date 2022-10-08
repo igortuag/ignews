5975d3fa88bdcd21512f497a15835a583bc9649f
Tuag <igortuag@gmail.com>
2022-10-08 15:23:35 -0300

    get user active subscription and return on session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,9 +19,29 @@ export const authOptions = {
     secret: process.env.JWT_SIGNING_PRIVATE_KEY,
   },
   callbacks: {
-    async session(session) {
+    async session(session) { 
       try {
-        return session;
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index("subscription_by_user_ref"),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(q.Index("user_by_email"), q.Casefold(session.user.email))
+                  )
+                )
+              ),
+              q.Match(q.Index("subscription_by_status"), "active"),
+            ])
+          )
+        );
+
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription,
+        };
       } catch {
         return {
           ...session,
