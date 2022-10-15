51e2d975c48f4b09420c2918360448b7e4de96d8
Tuag <igortuag@gmail.com>
2022-10-15 06:34:55 -0300

    add setup files after env

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testIgnorePatterns: ["<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   }
