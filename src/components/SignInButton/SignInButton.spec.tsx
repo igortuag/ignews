0856559527b762d10c0757484ed451bbd1dc1d01
Tuag <igortuag@gmail.com>
2022-10-19 09:09:23 -0300

    add mocked use session separetly

--- src/components/SignInButton/SignInButton.spec.tsx
+++ src/components/SignInButton/SignInButton.spec.tsx
@@ -1,16 +1,16 @@
 import { render, screen } from "@testing-library/react";
+import { mocked } from "ts-jest/utils";
+import { useSession } from "next-auth/react";
 import SignInButton from ".";
 
-jest.mock("next-auth/client", () => {
-  return {
-    useSession() {
-      return [null, false];
-    },
-  };
-});
+jest.mock("next-auth/client");
 
 describe("SignInButton component", () => {
   it("renders correctly when user is not authenticated", () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
     render(<SignInButton />);
 
     expect(screen.getByText("Sign in with Github")).toBeInTheDocument();
