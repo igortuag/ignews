e26c6ac9993fadbf911d12cf6b8e26c30d1b93cc
Tuag <igortuag@gmail.com>
2022-10-21 05:51:40 -0300

    add mock to next auth

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import SubscribeButton from ".";
 
+jest.mock('next-auth/client', () => { 
+  return {
+    useSession: () => [null, false],
+  }
+})
+
 describe("SubscribeButton component", () => {
   it("renders correctly", () => {
     render(<SubscribeButton priceId="123" />);
