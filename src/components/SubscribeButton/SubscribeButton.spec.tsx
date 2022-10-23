e374be402f429dd99de15c4c6fe6bf13763d0856
Tuag <igortuag@gmail.com>
2022-10-23 08:14:11 -0300

    add use ssesion mocked for logged test

--- src/components/SubscribeButton/SubscribeButton.spec.tsx
+++ src/components/SubscribeButton/SubscribeButton.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import SubscribeButton from ".";
-import { signIn } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { mocked } from "ts-jest/utils";
 
@@ -33,7 +33,20 @@ describe("SubscribeButton component", () => {
 
   it("redirects to posts when user already has a subscription", () => {
     const useRouterMocked = mocked(useRouter);
+    const useSessionMocked = mocked(useSession);
     const pushMock = jest.fn();
+
+    useSessionMocked.mockReturnValueOnce([
+      {
+        user: {
+          name: "John Doe",
+          email: "john@dow.com",
+        },
+        activeSubscription: "fake-active-subscription",
+        expires: "fake-expires",
+      },
+      false,
+    ]);
 
     useRouterMocked.mockReturnValueOnce({
       push: pushMock,
