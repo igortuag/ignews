571b8193396a06841d153dcfc490bdfa06a5cb54
Tuag <igortuag@gmail.com>
2022-10-24 09:04:43 -0300

    add mocks to test home

--- src/tests/Home.spect.tsx
+++ src/tests/Home.spect.tsx
@@ -1,6 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import Home from "../pages";
 
+jest.mock("next/router");
+jest.mock("next-auth/react", () => {
+  return {
+    useSession: () => [null, false],
+  };
+});
+
 describe("Home page", () => {
   it("renders correctly", () => {
     render(
