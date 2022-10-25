c1c3210eb3076458aab64f80f6cdc1acae30dcba
Tuag <igortuag@gmail.com>
2022-10-25 08:42:54 -0300

    add test loads initial data

--- src/tests/Home.spect.tsx
+++ src/tests/Home.spect.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
-import Home from "../pages";
+import { stripe } from "../services/stripe";
+import { mocked } from "ts-jest/utils";
+import Home, { getStaticProps } from "../pages";
 
 jest.mock("next/router");
 jest.mock("next-auth/react", () => {
@@ -7,6 +9,7 @@ jest.mock("next-auth/react", () => {
     useSession: () => [null, false],
   };
 });
+jest.mock("../../services/stripe");
 
 describe("Home page", () => {
   it("renders correctly", () => {
@@ -20,5 +23,27 @@ describe("Home page", () => {
     );
 
     expect(screen.getByText(/100/i)).toBeInTheDocument();
+  });
+
+  it("loads initial data", async () => {
+    const retrieveStripePricesMocked = mocked(stripe.prices.retrieve);
+
+    retrieveStripePricesMocked.mockResolvedValueOnce({
+      priceId: "123",
+      amount: "100",
+    } as any);
+
+    const response = await getStaticProps({} as any);
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: "123",
+            amount: "100",
+          },
+        },
+      })
+    );
   });
 });
