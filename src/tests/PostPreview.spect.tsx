8b1ed3cd9a16d26b3eca88b79eef1faeb5382c27
Tuag <igortuag@gmail.com>
2022-10-30 10:32:18 -0300

    add test to render correct post preview page

---
+++ src/tests/PostPreview.spect.tsx
@@ -0,0 +1,81 @@
+import { render, screen } from "@testing-library/react";
+import PostPreview, { getStaticPaths } from "../pages/posts/preview/[slug]";
+import { mocked } from "ts-jest/utils";
+
+import { prismic } from "../../services/prismic";
+import { getSession } from "next-auth/react";
+
+jest.mock("../../services/stripe");
+jest.mock("next-auth/react", () => {});
+jest.mock("../../services/prismic");
+
+const postPreview = {
+  slug: "my-new-postPreview",
+  title: "My new postPreview",
+  content: "<p>PostPreview excerpt</p>",
+  updatedAt: "10 de Abril",
+};
+
+describe("Post preview page", () => {
+  it("renders correctly", () => {
+    render(<PostPreview post={postPreview} />);
+
+    expect(screen.getByText(/My new postPreview/i)).toBeInTheDocument();
+    expect(screen.getByText(/Wanna continue reading?/i)).toBeInTheDocument();
+  });
+
+  // it("redirects user if no subscription is found", async () => {
+  //   const getSessionMocked = mocked(getSession);
+
+  //   getSessionMocked.mockReturnValueOnce([null, false]);
+
+  //   const response = await getServerSideProps({
+  //     params: { slug: "my-new-postPreview" },
+  //   } as any);
+
+  //   expect(response).toEqual(
+  //     expect.objectContaining({
+  //       redirect: expect.objectContaining({
+  //         destination: "/",
+  //       }),
+  //     })
+  //   );
+  // });
+
+  // it("loads initial data", async () => {
+  //   const getSessionMocked = mocked(getSession);
+  //   const getPrismicClientMocked = mocked(prismic);
+
+  //   getSessionMocked.mockReturnValueOnce([
+  //     { activeSubscription: "fake-active-subscription" },
+  //     false,
+  //   ] as any);
+
+  //   getPrismicClientMocked.mockReturnValueOnce({
+  //     getByUID: jest.fn().mockResolvedValueOnce({
+  //       data: {
+  //         title: [{ type: "heading", text: "My new postPreview" }],
+  //         content: [{ type: "paragraph", text: "PostPreview excerpt" }],
+  //       },
+  //       last_publication_date: "04-01-2021",
+  //     }),
+  //   } as any);
+
+  //   const response = await getServerSideProps({
+  //     params: { slug: "my-new-postPreview" },
+  //   } as any);
+
+  //   expect(response).toEqual(
+  //     expect.objectContaining({
+  //       props: {
+  //         postPreview: {
+  //           slug: "my-new-postPreview",
+  //           title: "My new postPreview",
+  //           content: "<p>PostPreview excerpt</p>",
+  //           updatedAt: "01 de abril de 2021",
+  //         },
+  //       },
+  //     })
+  //   );
+  // });
+});
