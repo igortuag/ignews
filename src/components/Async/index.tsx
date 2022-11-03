4724b8e80dcd125b1ad65435dae419ed6cbba869
Tuag <igortuag@gmail.com>
2022-11-03 08:02:06 -0300

    add new invisible button element to test

--- src/components/Async/index.tsx
+++ src/components/Async/index.tsx
@@ -2,10 +2,12 @@ import { useEffect, useState } from "react";
 
 export function Async() {
   const [isButtonVisible, setIsButtonVisible] = useState(false);
+  const [isButtonInvisible, setIsButtonInvisible] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
       setIsButtonVisible(true);
+      setIsButtonInvisible(true);
     }, 1000);
   }, []);
 
@@ -13,6 +15,7 @@ export function Async() {
     <div>
       <div>Hello World</div>
       {isButtonVisible && <button>Button</button>}
+      {isButtonInvisible && <button>Invisible Button</button>}
     </div>
   );
 }
